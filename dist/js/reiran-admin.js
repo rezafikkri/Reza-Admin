196e302170133187ee8f681ad343ebe52ec4b6c8
rezafikkri <fikkri.reza@gmail.com>
2020-05-27 21:58:39 +0700

    memperbaiki responsive footer, header, info-box dan navbar-top, membuat sidebar menu dropdown dan membuat table

--- dist/js/reiran-admin.js
+++ dist/js/reiran-admin.js
@@ -1,6 +1,7 @@
+const sidebar = document.querySelector("aside.sidebar");
+
 /* show hide menu sidebar */
 const navbarSidebarToggler = document.querySelector("a.navbar__sidebar-toggler");
-const sidebar = document.querySelector("nav.sidebar");
 const mainKonten = document.querySelector("main.main-konten");
 if(navbarSidebarToggler !== null) {
 	navbarSidebarToggler.addEventListener('click', function(e){
@@ -9,5 +10,20 @@ if(navbarSidebarToggler !== null) {
 
 		sidebar.classList.toggle("sidebar--show-sm");
 		mainKonten.classList.toggle("main-konten--show-sidebar");
+	});
+}
+
+/* sidebar dropdown */
+if(sidebar !== null) {
+	sidebar.addEventListener('click', function(e){
+		e.preventDefault();
+
+		let targetDropdown = e.target;
+		if(!targetDropdown.classList.contains("sidebar__btn-dropdown")) targetDropdown = e.target.parentElement;
+		if(targetDropdown.classList.contains("sidebar__btn-dropdown")) {
+			targetDropdown.classList.toggle("sidebar__btn-dropdown--active");
+			targetDropdown.querySelector("span.sidebar__span-dropdown").classList.toggle("sidebar__span-dropdown--rotate");
+			targetDropdown.nextElementSibling.classList.toggle("sidebar__dropdown--show");
+		}
 	});
 }
